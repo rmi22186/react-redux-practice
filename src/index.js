134f5cb07123febc39d2de6d9ef30debb9127824
Robert Ing <rob@thoughtindustries.com>
2016-02-20 16:18:02 +0000

    beginng through 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// create a new component and this component shoud produce some html
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyCs8erOdTQmU51nQalFssF18LKySS-b-zU';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+//take this component's generated HTML and put it on the page (in the DOM)
